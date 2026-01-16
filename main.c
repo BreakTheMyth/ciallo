765a38e8a96e8771715182d544f728b0cc1bad4c
BreakTheMyth <pinks302@163.com>
2026-01-16 12:42:33 +0800

    update demo

--- main.c
+++ main.c
@@ -1,39 +1,48 @@
 #include <stdio.h>
 #include "ciallo.h"
 
-void test1(void *arg) {
-    Ciaolima;
+/**
+ * test1 receives NULL
+ */
+void test1(void *arg);
 
-    for (int i = 0; i < 10; i++) {
-        printf("test1:%d\n", i);
-        Ciallo_;
-    }
+/**
+ * test2 receives a int pointer,
+ * the pointer stores the return value address
+ */
+void test2(void *arg);
 
-    Ciavanllo;
-}
+/**
+ * test3 receives a struct pointer, 
+ * member1 and member2 are parameters, 
+ * member3 stores the return value address
+ */
+void test3(void *arg);
 
-void test2(void *arg) {
-    Ciaolima;
-
-    for (int i = 0; i < 10; i++) {
-        printf("test2:%d\n", i);
-        Ciallo_;
-    }
-
-    Ciavanllo;
-}
-
+/**
+ * main->(init)
+ * ->main->(call test1)
+ * ->test1->(init)
+ * ->main->(switch)
+ * ->test1->(switch)
+ * ->main->...
+ * ->main->(switch)
+ * ->test1->(exit)
+ * ->main->...
+ */
 int main(void) {
-    Ciaolima;
+    Ciaolima; // init
 
-    Ciallo(test1, NULL);
+    Ciallo(test1, NULL); // call test1
 
     for (int i = 0; i < 10; i++) {
         printf("main:%d\n", i);
-        Ciallo_;
+        Ciallo_; // switch to another function
     }
 
-    Ciallo(test2, NULL);
+    int result;
+
+    Ciallo(test2, &result); // call test2
 
     Ciallo_;
 
@@ -43,6 +52,69 @@ int main(void) {
     }
 
     Ciallo_;
+    Ciallo_;
+    Ciallo_;
+    Ciallo_;
+    Ciallo_;
+    Ciallo_;
+
+    printf("main:test2_result=%d\n", result);
 
     return 0;
 }
+
+void test1(void *arg) {
+    Ciaolima;
+
+    for (int i = 0; i < 10; i++) {
+        printf("test1:%d\n", i);
+        Ciallo_;
+    }
+
+    Ciavanllo; // exit
+}
+
+void test2(void *arg) {
+    Ciaolima;
+
+    int result = 0;
+
+    for (int i = 0; i < 10; i++) {
+        result += i;
+        printf("test2:%d\n", result);
+        Ciallo_;
+    }
+
+    *(int *)arg = result;
+
+    result = 0;
+    struct {
+        int a;
+        int b;
+        int *res;
+    } arg_abaaba = {7, 21, &result};
+    
+    Ciallo(test3, &arg_abaaba);
+
+    Ciallo_;
+    Ciallo_;
+
+    printf("test1:test3_result=%d\n", result);
+
+    Ciavanllo;
+}
+
+void test3(void *arg) {
+    Ciaolima;
+
+    struct {
+        int a;
+        int b;
+        int *result;
+    } *arg_abaaba = arg;
+
+    *arg_abaaba->result = arg_abaaba->a + arg_abaaba->b;
+
+    Ciavanllo;
+}
+
